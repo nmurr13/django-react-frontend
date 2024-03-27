b24fdd941975fe37301f6ab2cae4d2979f13e470
Nicholas Murray <nmurr13@protonmail.com>
2024-03-27 15:36:37 -0700

    all done

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,4 +1,15 @@
+import { Link } from "react-router-dom"
 
-export default function Pet(props) {
-    return <h1>Pet {props.name}</h1>
+export default function Pet({ pet }) {
+
+    const id = pet.url.split('/')[4]
+
+    
+    return (
+        <div>
+            <Link to={`/pets/${id}`}>
+                <h1>{pet.name}</h1>
+            </Link>
+        </div>
+    )
 }
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,10 +1,40 @@
-function Show(props) {
+import { Link, useLoaderData, Form } from "react-router-dom"
 
+export default function Show(props) {
+
+    const aPet = useLoaderData()
+
+    // optional chaining ?.
+    const id = aPet?.url.split("/")[4]
     return (
-        <>
-            <h1>Show</h1>
-        </>
+        <div>
+            <h1>{aPet.name}</h1>
+            <h2>{aPet.age}</h2>
+
+            <div>
+                {/* Update form */}
+                <Form action={`/update/${id}/`} method="POST">
+                    <label htmlFor="name">
+                        Rename you Turtle
+                        <input type="text" id="name" name="name" defaultValue={aPet.name}/>
+                    </label>
+                    
+                    <label htmlFor="age">
+                        Did they have a birthday?
+                        <input type="number" id="age" name="age" defaultValue={aPet.age}/>
+                    </label>
+                    <button style={{"backgroundColor": "blue"}}>Update our Turtle</button>
+                </Form>
+
+                {/* Delete Form */}
+                <Form action={`/delete/${id}/`} method="post">
+                    <button style={{"backgroundColor": "red"}}>Release Turtle</button>
+                </Form>
+            </div>
+
+            <Link to="/">
+                <button>Go Back</button>
+            </Link>
+        </div>
     )
 }
-
-export default Show;
